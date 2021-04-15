ff6b1fff86d0f963ab58890cd0f8314262743a69
Roman Kohutyak <romankohutyak@gmail.com>
2021-04-15 22:40:08 +0300

    tests: more resorches needed on fib memo

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 // fib takes fib(a-1) + fib(a-2) = result
 
-function fib(num) {
-  if (num <= 1) {
-    return 1;
+// NBM (*not by myself*)
+function fib(num, previous = []) {
+  if (previous[num] !== undefined) {
+    return previous[num];
   }
 
-  return fib(num - 1) + fib(num - 2);
+  let result;
+
+  if (num <= 2) {
+    result = 1;
+  } else {
+    result = fib(num - 1, previous) + fib(num - 2, previous);
+  }
+
+  previous[num] = result;
+
+  return result;
 }
 
-console.log(fib(40));
+console.log(fib(5));
+// fib(5)
+// fib(5 - 1 = 4, []) + fib( 5 - 3 = 2, [])
+// fib(4 - 1 = 3, []) + 2
+// 3 + 2
+
+// Need to more explore
