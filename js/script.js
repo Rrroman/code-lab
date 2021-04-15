c2d0552921914f17e6304cf301c6f494c632a8d8
Roman Kohutyak <romankohutyak@gmail.com>
2021-04-15 22:05:25 +0300

    feat: factorial + memo testing

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
-// return new array, use element, index, array,
-// callback
+// const factorial = (n) => (n === 0 ? 1 : n * factorial(n - 1));
 
-const arr = [2, 4, 6, 7, 9, 21];
+// console.log(factorial(5));
 
-Array.prototype.myFilter = function myFilter(callback) {
-  const arr = [...this];
-  const tempArray = [];
+// function factorial(n) {
+//   if (n === 0) {
+//     return 1;
+//   }
 
-  for (let i = 0; i < arr.length; i++) {
-    if (callback(arr[i], i, arr)) {
-      tempArray.push(arr[i]);
-    }
+//   return n * factorial(n - 1);
+// }
+
+function factorial(n, product = 1) {
+  if (n === 0) {
+    return product;
   }
-  return tempArray;
-};
+  product = product * n;
+  return factorial(n - 1, product);
+  // return factorial(n - 1, product * n);
+}
 
-console.log(arr.myFilter((number) => number < 7));
+console.log(factorial(5));
+
+// ==========
+// factorial(5)
+//  5 * factorial(5 - 1)
+//   4 * factorial(4 - 1)
+//     3 * factorial(3 - 1)
+//      2 * factorial(2 - 1)
+//        1
+//      2 * 1 = 2
+//    3 * 2 = 6
+//   4 * 6 = 24
+//  5 * 24 = 120
+
+// ==========
+
+// factorial(5, (result = 1));
+//     factorial( 5 - 1 = 4, 5)
+//      factorial( 4 - 1 = 3, 5*4=20)
+//        factorial( 3 - 1 = 2, 20*3=60)
+//          factorial( 2 - 1 = 1, 60*2 = 120)
+//          factorial( 1, 120) = 120
