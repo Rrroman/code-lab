3203826f2a1a422f83d58a773a71e74198c399ec
Roman Kohutyak <romankohutyak@gmail.com>
2021-04-17 15:39:21 +0300

    feat: add 1 more setTimout

--- js/script.js
+++ js/script.js
@@ -9,9 +9,16 @@ bodyElement.appendChild(button);
 
 const getLocationHandler = () => {
   navigator.geolocation.getCurrentPosition(
-    (positionData) => console.log(positionData),
+    (positionData) => {
+      setTimeout(() => {
+        console.log('After setTimeout will be last');
+      }, 1000);
+      console.log(positionData);
+    },
     (error) => console.log(error)
   );
+
+  setTimeout(() => console.log('Obvious will be second'), 0);
 
   console.log('After getting location in code but prints before');
 };
