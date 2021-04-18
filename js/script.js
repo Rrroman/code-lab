a3d73d8319d7e34812f0aafc73ea0495e3f9809f
Roman Kohutyak <romankohutyak@gmail.com>
2021-04-18 23:28:41 +0300

    feat: currySum

--- js/script.js
+++ js/script.js
@@ -1,58 +1,12 @@
-const inc = (function () {
-  let counter = 0;
-  return function () {
-    counter += 1;
-    console.log(counter);
-    // return counter;
+function currySum(firstNumber) {
+  return (nextNumber) => {
+    if (nextNumber === undefined) {
+      return firstNumber;
+    }
+
+    return currySum(firstNumber + nextNumber);
   };
-})();
+}
 
-inc();
-inc();
-inc();
-inc();
-inc();
-
-//===============
-
-const obj = {
-  a: 42,
-  say: function () {
-    setTimeout(
-      // () => without arrow function
-      function () {
-        console.log(this.a);
-      }.bind(this)
-    );
-  },
-};
-
-obj.say();
-
-//=================
-
-const fruits = ['banana', 'cherry', 'grape', 'banana', 'banana', 'grape'];
-
-const sortFruits = (arr) => {
-  const fruitAmountObj = arr.reduce((obj, fruit) => {
-    obj[fruit] = obj[fruit] ? obj[fruit] + 1 : 1;
-    return obj;
-  }, {});
-
-  const objKeys = Object.keys(fruitAmountObj);
-  const sorted = objKeys.sort(
-    (keyValueSmaller, keyValueBigger) =>
-      fruitAmountObj[keyValueBigger] - fruitAmountObj[keyValueSmaller]
-  );
-  return sorted;
-};
-
-console.log(sortFruits(fruits));
-
-//================
-
-const multiplyByTwo = (...rest) => {
-  return rest.map((number) => number * 2);
-};
-
-console.log(multiplyByTwo(1, 2, 3, 4, 10));
+const result = currySum(1)(2)(3)(4)();
+console.log(result);
