3cc1b8188c9999b7ed50d61377c6c5398ca26103
Roman Kohutyak <romankohutyak@gmail.com>
2021-04-20 13:25:58 +0300

    feat: test with object keys Order (default sorting order)

--- js/script.js
+++ js/script.js
@@ -1,34 +1,10 @@
-let person = { name: 'Roman' };
-const cat = { name: 'Fluffy' };
-
-let friend = { name: 'Bob' };
-
-const group = new WeakSet();
-
-group.add(friend);
-console.log(group);
-
-// some operations ...
-friend = null;
-console.log(group);
-
-/**
- * new WeakSet() - release data for garbage collector so it can delete unused data/property,
- * browser decides when it wil be deleted.
- *
- * new Set() don't let garbage collector to delete, property will be deleted only if
- * we do this by ourself with help of .delete() method
- *
- * Same for WeakMap
- * also keys must be an object
- *
- * This structures is good when you don't want to delete manually clearing data'
- */
-
-const personData = new WeakMap();
-
-personData.set(person, 'Extra data here');
-
-person = null;
-
-console.log(personData);
+const objAZ = {
+  4: 32,
+  1: 100,
+  z: 'String key',
+  2: 'i am second',
+  3: 'this works because all keys are numbers',
+  a: 'strings keys not sorted alphabetically',
+  b: 123,
+};
+console.log(objAZ);
