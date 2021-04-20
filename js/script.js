67515baa5fca14f283bcf8750dfd4fba54886472
Roman Kohutyak <romankohutyak@gmail.com>
2021-04-20 09:52:29 +0300

    feat: some more filter duplicate versions

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
 const array = [1, 2, 2, 3, 4, 4, 4, 5, 5, 6, 7];
 
-const removeDuplicates = array.filter(
-  (number, index, arr) => arr.indexOf(number) === index
-);
-
-console.log(removeDuplicates);
-
 function removeDup(arr) {
   const filtered = [];
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr.indexOf(arr[i]) === i) {
+    if (filtered.indexOf(arr[i]) === -1) {
       filtered.push(arr[i]);
     }
   }
@@ -20,5 +14,14 @@ function removeDup(arr) {
 
 console.log(removeDup(array));
 
-const filtered = [...new Set(array)];
-console.log(filtered);
+const removeDuplicates = (arr) => {
+  const temp = [];
+
+  arr.forEach((number) => {
+    temp.indexOf(number) === -1 ? temp.push(number) : false;
+  });
+
+  return temp;
+};
+
+console.log(removeDuplicates(array));
